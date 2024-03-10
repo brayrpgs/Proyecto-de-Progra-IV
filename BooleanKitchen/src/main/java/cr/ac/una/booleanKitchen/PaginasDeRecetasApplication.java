726a75781a4a5730c119d400e6a01dc746c00ca6
BrayRPGs <85127778+brayrpgs@users.noreply.github.com>
2024-03-09 20:12:08 -0600

    Add files via upload

---
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/PaginasDeRecetasApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.booleanKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginasDeRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginasDeRecetasApplication.class, args);
+	}
+
+}
