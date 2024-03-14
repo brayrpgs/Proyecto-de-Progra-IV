c671d17312ae5ab06589ce86c11ee46b114da1af
Dj229 <Dj229@Maik24>
2024-03-14 16:53:57 -0600

    update

---
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/domain/User.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.booleanKitchen.domain;
+
+/**
+ *
+ * @author Maik
+ */
+public class User extends Person{
+    
+    private String username;
+    private String email;
+    private String password;
+    private String userType;
+    
+}
