a1872c72f6e1b877d6dda9fa5a0d04ff8be17f33
A.Briones <77174654+A-BrionesVargas@users.noreply.github.com>
2024-03-13 22:33:23 -0600

    Login
    Faltan validar:
    1-Que usuario y contraseña coincidan con la BD.
    2-Manejar en caso que el acceso no sea valido.
    3-Registrar usuario
    4-Cambiar la contraseña cuando la olvida

---
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/Controller/MainController.java
@@ -0,0 +1,18 @@
+
+package cr.ac.una.booleanKitchen.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Daniel Briones
+ */
+@Controller
+public class MainController {
+    @GetMapping({"/", "/home", "/index", "/login"})
+    public String inicio(){
+        System.out.println("Mostramos html de login");
+        return "login";
+    }
+}
