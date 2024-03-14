c42ec98c78dd531e7d0d134b7af3aca6e89aa57d
BrayRPGs <brayanymelquisedec@gmail.com>
2024-03-13 21:18:32 -0600

    update

---
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/domain/Person.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.booleanKitchen.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author BrayRPGs
+ */
+@Setter
+@Getter
+@ToString
+public class Person {
+    private String nationality;
+    private String phoneNumber;
+
+    public Person(String nationality, String phoneNumber) {
+        this.nationality = nationality;
+        this.phoneNumber = phoneNumber;
+    }
+    public Person() {
+    }
+}
