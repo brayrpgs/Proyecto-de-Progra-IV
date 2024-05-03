0332a732de166be0afb7b3ff3f842e0c073c72ed
KendallFallas07 <KendallFallas2018@gmail.com>
2024-05-03 01:01:17 -0600

    Cambiar el imagen

--- BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/Controller/MainController.java
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/Controller/MainController.java
@@ -1,8 +1,19 @@
 
 package cr.ac.una.booleanKitchen.Controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import cr.ac.una.booleanKitchen.service.ImagenService;
 
 /**
  *
@@ -15,4 +26,29 @@ public class MainController {
         System.out.println("cambio en rama de brayan");
         return "login";
     }
+
+    @GetMapping("/upload")
+    public String uploadImageForm() {
+        return "image/img"; // Este es el nombre de la plantilla Thymeleaf
+    }
+
+    @PostMapping("/upload")
+    public String uploadImage(@RequestParam("image") MultipartFile image) {
+        // Procesa la imagen aquí
+        
+        String url = "Proyecto-de-Progra-IV/BooleanKitchen/src/main/resources/static/img/prueba/kendallfallas2018@gmail.com/";
+        
+        try {
+            File destino = new File(url + image.getOriginalFilename());
+            destino.createNewFile();
+            image.transferTo(destino);
+            
+        } catch (Exception e) {
+           
+        }
+
+
+        return "image/img";
+    }
+
 }
--- BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/service/ImagenService.java
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/service/ImagenService.java
@@ -4,6 +4,7 @@
  */
 package cr.ac.una.booleanKitchen.service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,29 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
  * @author josue
  */
 public class ImagenService {
-    //aqui debemos decir la ruta donde las imagenes se van a guardar (preferiblemente carpeta static.img)
-     String uploadsDir = "src/main/resources/static";
-     
-      //metodo de ingreso de imagen a carpeta externa en proyecto
-      public String insertImg(MultipartFile file){
-            if (!file.isEmpty()) {
-            try {
-                // Ruta donde se guardará la imagen en el directorio de recursos estáticos
-               
-                byte[] bytes = file.getBytes();
-                Path path = Paths.get(uploadsDir + "/" + file.getOriginalFilename());
-                Files.write(path, bytes);
-                //retorna el nombre de la imagen
-                return file.getOriginalFilename();
-            } catch (IOException e) {
-                //por si da error
-                return "Error al subir la imagen.";
-            }
-        } else {
-                //retorna vacio si la imagen esta vacia, para validad
-            return " ";
-        }
-      }
     
+    public void insertImageToFolder(String url, File file){
+
+        System.out.println("Ruta:" + url);
+
+    }    
     
 }
