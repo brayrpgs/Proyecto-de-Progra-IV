9358c937e2852755ddc21d36e9c568218d40da33
BrayRPGs <BrayRPGs@DESKTOP-F0T3J8H>
2024-05-11 00:35:14 -0600

    update

--- BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/domain/ShopList.java
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/domain/ShopList.java
@@ -1,12 +1,13 @@
 package cr.ac.una.booleanKitchen.domain;
 
+import jakarta.persistence.Column;
 import java.util.Date;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,16 +22,26 @@ import lombok.ToString;
 @ToString
 @Builder
 @Entity
+@Table(name = "tb_Shoplist")
 public class ShopList {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
+    @Column(name = "amount", nullable = false)
     private Float amount;
+    @Column(name = "notes", length = Integer.MAX_VALUE, nullable = false)
     private String notes;
+    @Column(name = "brand", length = 50, nullable = false)
     private String brand;
+    @Column(name = "state", nullable = false)
     private Boolean state;
+    @Column(name = "date", nullable = false)
     private Date date;
+    @Column(name = "idUser", nullable = false, unique = true)
     private Integer idUser;
 
     public ShopList(Integer id, String name, Float amount, String notes, String brand, Boolean state, Date date,
