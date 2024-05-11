a75161d2f06a48abc9a5c9c6d8f492951d1cf384
BrayRPGs <85127778+brayrpgs@users.noreply.github.com>
2024-05-10 23:38:43 -0600

    daily update

---
+++ BooleanKitchen/target/classes/static/js/shoplist/shoplist.js
@@ -0,0 +1,35 @@
+/**
+ * Function to navigate to the insert page from the backend.
+ *
+ * @param {string} value - The URL to navigate to.
+ */
+function goTo(value) {
+    window.location.href = value;
+    return;
+}
+
+function showComponentInsert() {
+    if (document.getElementById("insert").getAttribute("value") === "0") {
+        document.getElementById("insert").setAttribute("value", "1");
+        document.getElementById("insert").innerHTML = "Cancelar Compra";
+        document.getElementById("insert").style.color = "Orange";
+        //ajax
+        let http = new XMLHttpRequest();
+        let url = "/shoplist/insert";
+        http.open("GET", url, true);
+        http.send();
+        http.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("insertData").innerHTML = this.responseText;
+            }
+        };
+        return;
+    }
+    else {
+        document.getElementById("insert").setAttribute("value", "0");
+        document.getElementById("insertData").innerHTML = "";
+        document.getElementById("insert").innerHTML = "Agregar Compra";
+        document.getElementById("insert").style.color = "Blue";
+        return;
+    }
+}
