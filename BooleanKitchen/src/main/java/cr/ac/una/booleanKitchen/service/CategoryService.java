c1d9b4882469202b7e896ca7eb444720553b6d16
joss_Rojas.cr <josueporrasrojas8@gmail.com>
2024-05-21 08:13:38 -0600

    arreglo de warnings

--- BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/service/CategoryService.java
+++ BooleanKitchen/src/main/java/cr/ac/una/booleanKitchen/service/CategoryService.java
@@ -101,7 +101,7 @@ public class CategoryService implements ICategoryService{
     }
       //Por el momento tengo las eqtiquetas en Backend, mas adelante las implemento en BD.
       public LinkedList<String> getListLabel(){
-          LinkedList<String> list= new LinkedList();
+          LinkedList<String> list= new LinkedList<String>();
           list.add("INGREDIENTE");
           list.add("UTENCILIO");
           list.add("RECETAS");
